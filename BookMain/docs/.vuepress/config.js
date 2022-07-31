b37b3d2c4d36cb08bee7a1c46f0cefec13ba143b
mobe <mobetax81072@gmail.com>
2022-08-01 03:27:36 +0800

    新增 Docker Init

---
+++ BookMain/docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    title:'我的 Docker 筆記',
+    description:'一個不成材的工程師的 Docker 筆記',
+    base: '/BookDocker/',
+    // theme: "book",
+    themeConfig: {
+        locales:{
+            '/':{
+                sidebar: [
+                  ['/chapter1/one', '第一章'],
+                  ['/chapter1/two', '第二章'],
+                    {
+                      title: '第一章',
+                      collapsable: false,
+                      children: [
+                        ['/chapter1/one', '第一小節'],
+                        ['/chapter1/two', '第二小節'],
+                      ]
+                    },
+                    {
+                        title: '第二章',
+                        collapsable: false,
+                        children: [
+                          ['/chapter2/one', '第一小節'],
+                          ['/chapter2/two', '第二小節'],
+                        ]
+                      }
+                ]
+            }
+        }
+    }
+}
---
+++ BookMain/docs/README.md
@@ -0,0 +1,29 @@
+# 我的 Docker 筆記
+
+一個不成材的工程師  
+「合抱之木，生於毫末；九層之臺，起於累土；千里之行，始於足下」  
+紀錄一下自身對於 Docker; Container; image 相關知識的理解  
+找一個比熟的東西交叉才不會感到無趣...筆記轉移中
+
+## Docker
+
+![DockerLogo](../docs/.vuepress/public/Docker_logo.png)  
+[官方網站傳送門](https://www.docker.com/)
+
+## Introduction
+
+:blue_book: 章節概述
+
+:wrench: 相關建議
+
+* Linux 基本使用
+* 認同容器化技術，對開發人員帶來一定的好處
+* 容器化技術絕對不是由 Docker 獨佔，Docker 只是一個良好的工具，輔助使用容器化技術
+
+```Javascript
+console.log(" Hello Docker !");
+console.log(" Build, Ship and Run !");
+console.log(" Build once, Run anywhere !");
+```
+
+Thank you!
